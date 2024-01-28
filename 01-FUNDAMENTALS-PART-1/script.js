2afa09d3dedda2c174d1469683b0563a424d8a9c
MinhVo <minhvo4398@gmail.com>
2024-01-28 14:06:38 +0700

    variable in js

--- 01-FUNDAMENTALS-PART-1/script.js
+++ 01-FUNDAMENTALS-PART-1/script.js
@@ -1,4 +1,28 @@
 let js = 'amazing';
-if(js=== 'amazing') alert('Javascript is FUN');
-
 console.log( 40 +8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matida = "JM"
+let $function = 27;
+let name = "Jonas"
+
+let person = 'jonas';
+let PI =3.14015
+
+//More readble
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+//Not readble
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob)
