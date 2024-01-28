17ba619ed1bcd5d2b475ed5da80b3fbfd79f67f3
MinhVo <minhvo4398@gmail.com>
2024-01-28 20:53:17 +0700

    opearator

---
+++ 01-FUNDAMENTALS-PART-1/operator.js
@@ -0,0 +1,15 @@
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas *2, ageJonas /10, 2**3);
+// 2**3 means 2 to the power of 3 = 2 *2 *2
+
+const firstName = 'Jonas';
+const lastName = 'Schedtmann'
+console.log(firstName + ' ' +  lastName);
+
+
+
+
