ab88fd531b501b4035774f9da66b0870657b113a
Minh Vo <minh.vo@noveogroup.com>
2024-01-29 14:25:47 +0700

    operator precedence

---
+++ 01-FUNDAMENTALS-PART-1/operator_precedence.js
@@ -0,0 +1,17 @@
+const now = 2037;
+const ageJonas = now -1991;
+const ageSarah = now - 2018;
+
+console.log(now -1991 > now - 2018)
+
+console.log(25-10-5);
+
+let x, y;
+x = y = 25 -10-5; // x = y = 10, x = 10
+console.log(x,y); //10 10
+
+const averageAge = ageJonas + ageSarah / 2;
+console.log(ageJonas, ageSarah, averageAge)// 46 19 55.5
+
+const averageAge1 = (ageJonas + ageSarah)/2;
+console.log(ageJonas, ageSarah, averageAge1)//46 19 32.5
