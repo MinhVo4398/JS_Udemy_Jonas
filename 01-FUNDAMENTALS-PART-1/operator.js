ff38a210a8a7655b76f2933819f55984217218a5
Minh Vo <minh.vo@noveogroup.com>
2024-01-29 09:08:21 +0700

    basic operator

--- 01-FUNDAMENTALS-PART-1/operator.js
+++ 01-FUNDAMENTALS-PART-1/operator.js
@@ -1,3 +1,4 @@
+//Math operators
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -10,6 +11,22 @@ const firstName = 'Jonas';
 const lastName = 'Schedtmann'
 console.log(firstName + ' ' +  lastName);
 
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10
+x *= 4; // x = x *4= 100
+x++; // x = x+1
+x--;
+x--;
+console.log(x) 
+
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >=18)
+
+const isFullAge = ageSarah >=18;
+console.log(now-1991 > now -2018 )
 
 
 
