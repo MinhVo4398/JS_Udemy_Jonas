a94f4e4d2b7e9c21b3aeda490c0152fa5689ea09
Minh Vo <minh.vo@noveogroup.com>
2024-01-30 11:18:04 +0700

    type coercion

--- type_conversion.js
+++ type_conversion.js
@@ -1,3 +1,4 @@
+// type conversion
 const inputYear = '1991';
 console.log(Number(inputYear)); // convert strign to number -1991(number)
 console.log(inputYear + 18); //199118 (string)
@@ -7,3 +8,16 @@ console.log(Number('Jonas')); //NaN (means invalid number)
 console.log(typeof NaN); //number
 
 console.log(String(23)); //string
+
+//type coercion
+console.log('I am ' + 23 + ' years old'); ///type coercion
+console.log('I am ' + String(23) + ' years old');
+console.log('23' - '10' -3); //10 (number)
+console.log('23' + '10' + 3); //23103 (string)
+console.log('23' * '2') ; //46 (number)
+
+let n = '1' + 1; //'11' (string)
+n = n -1; //10 (convert to number)
+console.log(n); //10
+
+console.log(2+3+4+'5'); //'95' (string)
