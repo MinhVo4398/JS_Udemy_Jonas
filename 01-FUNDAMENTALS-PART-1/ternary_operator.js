b79f694e32b1528d9ab87c96e7e7e547c6449c5a
Minh Vo <minh.vo@noveogroup.com>
2024-01-31 14:51:49 +0700

    ternary operator

---
+++ 01-FUNDAMENTALS-PART-1/ternary_operator.js
@@ -0,0 +1,17 @@
+const age = 23;
+//age >=18 ? console.log('I lkie to drink wine'):console.log('I lkie to drink water');
+
+const drink =  age > 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if(age >=18) {
+    drink2 = 'wine';
+}
+else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+
+
