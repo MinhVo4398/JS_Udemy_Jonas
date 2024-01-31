388363b670994fc567551369b32deec5b7e2f241
Minh Vo <minh.vo@noveogroup.com>
2024-01-31 13:08:48 +0700

    coding challenge 3

---
+++ 01-FUNDAMENTALS-PART-1/coding_challenge_3.js
@@ -0,0 +1,33 @@
+/*
+const scoreDolphins = (96 + 108 + 89) /3;
+const scoreKoalas = (88 + 100 + 110) /3;
+console.log(scoreDolphins, scoreKoalas);
+
+if(scoreDolphins > scoreKoalas) {
+    console.log('Dolphin win the trophy');
+}
+else if(scoreKoalas > scoreDolphins) {
+    console.log('Koalas  win the trophy')
+}
+else if(scoreDolphins === scoreKoalas) {
+    console.log('Both win the trophy!');
+}
+*/
+
+//BONUS 1
+const scoreDolphins = (97 + 112 + 81) /3;
+const scoreKoalas = (109 + 95 + 86) /3;
+console.log(scoreDolphins, scoreKoalas);
+
+if(scoreDolphins > scoreKoalas && scoreDolphins >=100) {
+    console.log('Dolphin win the trophy');
+}
+else if(scoreKoalas > scoreDolphins && scoreKoalas >=100) {
+    console.log('Koalas  win the trophy')
+}
+else if(scoreDolphins === scoreKoalas && scoreDolphins >=100 && scoreKoalas >=100) {
+    console.log('Both win the trophy!');
+}
+else {
+    console.log('No one wins the trophy')
+}
