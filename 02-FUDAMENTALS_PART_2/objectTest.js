e8dd0795c6eede001ab43a8ad8723e817cc74557
Minh Vo <minh.vo@noveogroup.com>
2024-02-19 18:01:47 +0700

    object

---
+++ 02-FUDAMENTALS_PART_2/objectTest.js
@@ -0,0 +1,17 @@
+// Use Array for order data
+const jonasArray = [
+    'Jonas',
+    'Schemedth'
+];
+
+// Use Object for un-structure data
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schemedman',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends:
+        [
+            'Micheal', 'Peter', 'Steven'
+        ]
+}
