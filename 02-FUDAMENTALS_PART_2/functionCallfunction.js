66cd7dc86c146136cb61ec7593eac88b37469776
Minh Vo <minh.vo@noveogroup.com>
2024-02-19 13:59:46 +0700

    calling function in other function

---
+++ 02-FUDAMENTALS_PART_2/functionCallfunction.js
@@ -0,0 +1,14 @@
+
+function cutFruitPieces(fruit) {
+    return fruit * 3;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));
