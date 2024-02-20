04e24753a2f1c46b264cc535c75ed4f8c411ec74
Minh Vo <minh.vo@noveogroup.com>
2024-02-20 12:07:58 +0700

    object test

--- 02-FUDAMENTALS_PART_2/objectTest.js
+++ 02-FUDAMENTALS_PART_2/objectTest.js
@@ -15,3 +15,26 @@ const jonas = {
             'Micheal', 'Peter', 'Steven'
         ]
 }
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas[`first`+ nameKey]);
+console.log(jonas['last' + nameKey]);
+/*
+const interestedIn  = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
+console.log(jonas[interestedIn]); // parse job -> teacher, parse location -> undefined
+
+if(jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+}
+else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job and friends');
+}
+*/
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonaschemdman';
+console.log(jonas)
+
