8e2e281d92e9cde03ee43a4f62fe3fafa2212dd8
minhvo <minhvo4398@gmail.com>
2024-03-02 17:08:03 +0700

    loop in JS

---
+++ 02-FUDAMENTALS_PART_2/loop.js
@@ -0,0 +1,6 @@
+// console.log('Lifting weights repetition 1');
+
+// for loop keeps running while condition is TRUE
+for(let rep =1 ; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
